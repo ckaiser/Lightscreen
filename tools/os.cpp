7cb2c1b30b10bc3d77d72929dfcf58e6a9a11a16
Christian Kaiser <info@ckaiser.com.ar>
2021-04-24 19:21:38 -0300

    Removed all copyright years from comments

--- tools/os.cpp
+++ tools/os.cpp
@@ -1,5 +1,5 @@
 ﻿/*
- * Copyright (C) 2017  Christian Kaiser
+ * Copyright (C) Christian Kaiser
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
